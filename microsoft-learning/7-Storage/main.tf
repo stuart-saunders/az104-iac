3a1a2dcb2e0a6b9886e33a8c7f6b1f36dd94255e
Stuart Saunders <github@stuartsaunders.net>
2022-07-01 21:32:10 +0100

    MSLearning - Lab 7 - Task 3

--- microsoft-learning/7-Storage/main.tf
+++ microsoft-learning/7-Storage/main.tf
@@ -10,10 +10,33 @@ resource "azurerm_resource_group" "rg1" {
 
 module "win_vm" {
   source  = "./modules/windows-vm"
-  vm_name = "vm0"
+  vm_name = "${var.prefix}-vm0"
 
   ip_configuration_name = "ipconfig"
   subnet_id             = azurerm_subnet.this.id
   location              = azurerm_resource_group.rg0.location
   resource_group_name   = azurerm_resource_group.rg0.name
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = "ssaz104msllab7${formatdate("DDMMhhmm", timestamp())}"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = "lab7"
+  storage_account_name  = azurerm_storage_account.this.name
+}
+
+resource "azurerm_storage_blob" "this" {
+  name                   = "licences/LICENCE"
+  storage_account_name   = azurerm_storage_account.this.name
+  storage_container_name = azurerm_storage_container.this.name
+  type                   = "Block"
+  source                 = "LICENCE"
+  content_type           = "text/plain"
+  access_tier            = "Hot"
 }
