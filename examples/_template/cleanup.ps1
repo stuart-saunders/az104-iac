c7e7e19119d53d6c0eb074b3524c44e6db9772ea
Stuart Saunders <github@stuartsaunders.net>
2022-09-24 11:18:58 +0100

    cleanup

--- examples/_template/cleanup.ps1
+++ examples/_template/cleanup.ps1
@@ -1,7 +1,3 @@
 terraform destroy --auto-approve
 Remove-Item -Recurse -Force .terraform*
 Remove-Item -Recurse -Force terraform.tfstate*
-
-# terraform destroy --auto-approve
-# rm -rf .terraform*
-# rm -rf terraform.tfstate*
