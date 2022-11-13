d034748fb660fec4f32a71f7e83f4551539fe2be
Stuart Saunders <github@stuartsaunders.net>
2022-11-13 21:58:07 +0000

    remove file

--- labs/microsoft-learning/8-VMs/infra/install_iis.ps1
+++
@@ -1,3 +0,0 @@
-Install-WindowsFeature -name Web-Server -IncludeManagementTools
-Remove-Item -Path 'C:\inetpub\wwwroot\iisstart.htm'
-Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value "$env:computername"
